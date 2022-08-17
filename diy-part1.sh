cb3caeaf1457b7acd44b69fdd415c7c723c0c735
Worship0 <37341209+Worship0@users.noreply.github.com>
2022-08-17 19:15:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #
+# Copyright (c) 2019-2020
+#
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
@@ -13,9 +15,3 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-
-# Modify default IP
-sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
-
-# Clear the login password
-sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
